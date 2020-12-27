702fefd85ed73cee9c49dba166e1927298df476a
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2020-12-27 16:42:57 +0100

    Periodically check battery state

--- cmd/save-my-battery.go
+++ cmd/save-my-battery.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"math"
 	"strconv"
+	"time"
 
 	"github.com/distatus/battery"
 	"github.com/gen2brain/beeep"
@@ -16,13 +17,15 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for i, battery := range batteries {
-		levelFloat := math.Floor(battery.Current / battery.Full * 100)
-		levelString := strconv.Itoa(int(levelFloat))
-		fmt.Printf("Battery number: %d, charge level: %v", i, levelString)
-		if err := beeep.Alert("Battery Level", levelString, ""); err != nil {
-			log.Fatal(err)
+	for {
+		for i, battery := range batteries {
+			levelFloat := math.Floor(battery.Current / battery.Full * 100)
+			levelString := strconv.Itoa(int(levelFloat))
+			fmt.Printf("Battery number: %d, charge level: %v", i, levelString)
+			if err := beeep.Alert("Battery Level", levelString, ""); err != nil {
+				log.Fatal(err)
+			}
 		}
+		time.Sleep(time.Second * 5)
 	}
-
 }
